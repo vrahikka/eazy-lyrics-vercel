71847baf1eac24e862d62b8e1608bd23661e9b74
Ville Rahikka <ville.rahikka@nitor.com>
2024-10-23 11:29:57 +0300

    Update to ESM

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ const primaryDark = '#EB0052';
 const gray = '#A6A6A6';
 const lightGray = '#CACACA';
 
-module.exports = {
+export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -18,20 +18,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-    },
-    colors: {
-      transparent: 'transparent',
-      primary: '#FF397F',
-      secondary: '#45CAFF',
-      white: '#FAFAFA',
-      gray,
-      lightGray,
-      black: '#0D0D0D',
-      dark: '#272727',
-      error: '#FF3434',
-      button: {
-        hoverBackgroundGray: lightGray,
-        hoverBackgroundPrimary: primaryDark,
+      colors: {
+        transparent: 'transparent',
+        primary: '#FF397F',
+        secondary: '#45CAFF',
+        white: '#FAFAFA',
+        gray,
+        lightGray,
+        black: '#0D0D0D',
+        dark: '#272727',
+        error: '#FF3434',
+        button: {
+          hoverBackgroundGray: lightGray,
+          hoverBackgroundPrimary: primaryDark,
+        },
       },
     },
   },
